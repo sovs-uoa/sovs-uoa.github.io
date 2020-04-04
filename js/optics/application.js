c2e52799fa6b01319f3dbe458de66d38c5601cf9
jtur044 <j.turuwhenua@auckland.ac.nz>
2020-04-04 14:42:27 +1300

    lab3 enabled

--- js/optics/application.js
+++ js/optics/application.js
@@ -24,8 +24,8 @@
                       { id: "7",  filename: "./lenses/galilean-telescope.lens", title: "Galilean Telescope"  },                    
                       { id: "10", filename: "./lenses/legrand-relaxed-schematic-eye.lens", title: "LeGrand Relaxed Schematic Eye (Relaxed)" },
                       { id: "11", filename: "./lenses/legrand-relaxed-schematic-eye-no-retina.lens", title: "LeGrand Relaxed Schematic Eye (Relaxed / No Retina)" },
-                      { id: "12", filename: "./lenses/reduced-eye-with-accommodation.lens", title: "Reduced Eye with Accommodation" },
-                      { id: "13", filename: "./lenses/reduced-eye-with-accommodation-img.lens", title: "Reduced Eye with Accommodation (Img)" }];
+                      { id: "12", filename: "./lenses/reduced-eye-with-accommodation-img.lens", title: "Reduced Eye with Accommodation" }];
+                      // { id: "12", filename: "./lenses/reduced-eye-with-accommodation.lens", title: "Reduced Eye with Accommodation" },
 
 
     displayOptions = { height             : 0.05, 
