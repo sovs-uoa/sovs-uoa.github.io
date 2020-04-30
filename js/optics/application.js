2a318ca424a7c14a9cdb8f33236ed3b4840ab2c2
jtur044 <j.turuwhenua@auckland.ac.nz>
2020-05-01 10:14:08 +1200

    added lab4 active updated

--- js/optics/application.js
+++ js/optics/application.js
@@ -25,7 +25,7 @@
                       { id: "10", filename: "./lenses/legrand-relaxed-schematic-eye.lens", title: "LeGrand Relaxed Schematic Eye (Relaxed)" },
                       { id: "11", filename: "./lenses/legrand-relaxed-schematic-eye-no-retina.lens", title: "LeGrand Relaxed Schematic Eye (Relaxed / No Retina)" },
                       { id: "12", filename: "./lenses/reduced-eye-with-accommodation-img.lens", title: "Reduced Eye with Accommodation" },
-                      { id: "13", filename: "./lenses/reduced-eye-with-ametropia.lens", title: "Reduced Eye (Lab 4)" },
+                      { id: "13", filename: "./lenses/reduced-eye-with-ametropia.lens",   title: "Reduced Eye (Lab 4)" },
                       { id: "14", filename: "./lenses/mystery-eye-with-ametropia-1.lens", title: "Mystery Eye with Ametropia #1" },
                       { id: "15", filename: "./lenses/mystery-eye-with-ametropia-2.lens", title: "Mystery Eye with Ametropia #2" }];
 
